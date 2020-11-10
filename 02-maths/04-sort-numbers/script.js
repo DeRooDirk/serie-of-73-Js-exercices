0a2654ae92b4981b6b511284c1cad2e825492f9f
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-10 16:18:34 +0100

    update Js
    finish exercise 3.2

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+/*(function() {
 
     // to get the value of an input: document.getElementById("element-id").value
 
@@ -20,13 +20,14 @@
     });
 
 })();
+*/
 
-(function() { 
+(function() {
     document.getElementById("run").addEventListener("click", function() {
         //The .map function in this case goes over every element (represented by the x and converts it to an integer).     
-        var numbers = document.getElementById("numbers").value.split(', ').map(x=>+x);         
+        var numbers = document.getElementById("numbers").value.split(', ').map(x => +x);
         // we need to add a function by parameter that sorts numerically.
-        numbers = numbers.sort(function(a, b){return a-b});
-        alert("Sorted numbers:" + numbers.toString());        
+        numbers = numbers.sort(function(a, b) { return a - b });
+        alert("Sorted numbers:" + numbers.toString());
     });
 })();
--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -9,8 +9,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
+function getColor() {
     // your code here
-
-})();
+    var color = document.getElementById("color").value;
+    document.body.style.background = color
+};
