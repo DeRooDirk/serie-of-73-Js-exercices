1de8ea059e54f7cbb8fd609fb3738d9a10b08d65
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-24 11:21:47 +0100

    update js
    a solution for 7-8

--- 07-dom/08-generate-table-two/script.js
+++ 07-dom/08-generate-table-two/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
 
-    // your code here
+    let container = document.getElementById("target");
+    let table = document.createElement("table");
+    let bodyTable = document.createElement("tbody");
+
+
+
+    //Creating the rows and cells
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement("tr");
+        for (let dirk = 0; dirk < 10; dirk++) {
+            let column = document.createElement("td");
+            column.innerHTML = (i + 1) * (dirk + 1);
+            row.appendChild(column);
+        }
+        bodyTable.appendChild(row);
+    }
+
+    table.appendChild(bodyTable);
+
+    container.appendChild(table);
 
 })();
