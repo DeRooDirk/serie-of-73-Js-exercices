23d8a10926a98882762e6c6f146d9156f86e1e2f
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-24 10:48:07 +0100

    updte js
    solution 7.7

--- 07-dom/07-generate-table-one/script.js
+++ 07-dom/07-generate-table-one/script.js
@@ -11,6 +11,24 @@
 
 (function() {
 
-    // your code here
+
+    let container = document.getElementById("target");
+    let table = document.createElement("table");
+    let bodyTable = document.createElement("tbody");
+
+
+
+    //Creating the rows and cells
+    for (i = 0; i < 10; i++) {
+        let row = document.createElement("tr");
+        let column = document.createElement("td");
+
+        column.innerHTML = "I haven’t failed. I’ve just found 10,000 ways that won’t work..– Thomas Edison";
+        row.appendChild(column);
+        bodyTable.appendChild(row);
+    }
+
+    table.appendChild(bodyTable);
+    container.appendChild(table);
 
 })();
